9ab89da0cefe8ed620dc210233bfc21467bf940f
Pranjal <g31.pranjal@gmail.com>
2015-08-11 11:31:16 +0530

    new page, few corrections

--- assets/js/resto.js
+++ assets/js/resto.js
@@ -51,7 +51,8 @@ function initialize() {
 	var map = new google.maps.Map(document.getElementById('map-canvas'),
 	    mapOptions);
 }
-//google.maps.event.addDomListener(window, 'load', initialize);
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
 $(function () {
